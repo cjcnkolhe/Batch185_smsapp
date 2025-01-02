b52945042f70de1a35c7e9301dfa068fa9a929cc
cjcnkolhe <cjcnkolhe@gmail.com>
2025-01-02 09:11:19 +0530

    add fees and pagin and remove

--- StudentMangamentSystem/src/main/java/edu/cjc/sms/app/servicei/StudentServiceI.java
+++ StudentMangamentSystem/src/main/java/edu/cjc/sms/app/servicei/StudentServiceI.java
@@ -9,5 +9,9 @@ public interface StudentServiceI {
 	List<Student> addStudent(Student s);
 	List<Student> getAllStudents();
 	List<Student> getBatchesStudent(String batchNumber);
+	Student getStudent(int id);
+	List<Student> updateStudentFees(int id, float amt);
+	void removeStudent(int id);
+	List<Student> paging(int pageNo, int i);
 
 }
--- StudentMangamentSystem/src/main/java/edu/cjc/sms/app/serviceimpl/StudentServiceImpl.java
+++ StudentMangamentSystem/src/main/java/edu/cjc/sms/app/serviceimpl/StudentServiceImpl.java
@@ -1,22 +1,29 @@
 package edu.cjc.sms.app.serviceimpl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableArgumentResolver;
 import org.springframework.stereotype.Service;
 
 import edu.cjc.sms.app.model.Student;
 import edu.cjc.sms.app.repositary.StudentRepo;
 import edu.cjc.sms.app.servicei.StudentServiceI;
+
 @Service
-public class StudentServiceImpl implements StudentServiceI{
+public class StudentServiceImpl implements StudentServiceI {
 
 	@Autowired
 	StudentRepo sr;
-	
+
 	@Override
 	public List<Student> addStudent(Student s) {
-		  sr.save(s);		
+		sr.save(s);
 		return sr.findAll();
 	}
 
@@ -28,9 +35,42 @@ public class StudentServiceImpl implements StudentServiceI{
 
 	@Override
 	public List<Student> getBatchesStudent(String batchNumber) {
-	
+
 		return sr.findAllByBatchNumber(batchNumber);
 	}
 
-	
+	@Override
+	public Student getStudent(int id) {
+
+		return sr.findById(id).get();
+	}
+
+	@Override
+	public List<Student> updateStudentFees(int id, float amt) {
+
+		Optional<Student> op = sr.findById(id);
+		if (op.isPresent()) {
+			Student s = op.get();
+			s.setFeesPaid(s.getFeesPaid() + amt);
+			sr.save(s);
+		}
+
+		return sr.findAll();
+	}
+
+	@Override
+	public void removeStudent(int id) {
+
+		sr.deleteById(id);
+	}
+
+	@Override
+	public List<Student> paging(int pageNo, int pageSize) {
+
+		Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by("studentFullName").ascending());
+		List<Student> list = sr.findAll(pageable).getContent();
+
+		return list;
+	}
+
 }
